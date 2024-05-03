d906a5c450c76fe1983fc806efe630f48176f6a6
Lhizel Tabual <lhizel.0415@gmail.com>
2024-05-04 01:13:47 +0800

    Initial Commit

---
+++ Module05Forms/PersonalInfoPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module05Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalInfoPage : ContentPage
+    {
+        public PersonalInfoPage()
+        {
+            InitializeComponent();
+
+            var genderList = new List<string>();
+            genderList.Add("Male");
+            genderList.Add("Female");
+            genderList.Add("Other");
+
+            var picker = new Picker { Title = "Gender"};
+            picker.ItemsSource = genderList;
+        }
+
+    DatePicker datePicker = new DatePicker
+        {
+            MinimumDate = new DateTime(2018, 1, 1),
+            MaximumDate = new DateTime(2018, 12, 31),
+            Date = new DateTime(2018, 6, 21)
+        };
+
+        private async void btnSubmit(object sender, EventArgs e)
+        {
+            await DisplayAlert("Form Submitted", "Your form has been submitted. Thank you!", "OK");
+        }
+
+    }
+}
